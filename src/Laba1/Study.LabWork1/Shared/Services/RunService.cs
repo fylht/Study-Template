d2c758aea2ca89a5b5dcbaf42b243e021236082d
Andrey <Omelnitskij@gmail.com>
2026-03-30 22:01:09 +0300

    сделана база для node

--- src/Laba1/Study.LabWork1/Shared/Services/RunService.cs
+++ src/Laba1/Study.LabWork1/Shared/Services/RunService.cs
@@ -1,3 +1,4 @@
+using Study.LabWork1.Features.Task3.Services;
 using Study.LabWork1.Shared.Abstractions;
 
 namespace Study.LabWork1.Shared.Services;
@@ -20,5 +21,27 @@ public class RunService : IRunService
     /// <summary>
     /// Задание 3
     /// </summary>
-    public void RunTask3() => throw new NotImplementedException();
+    public void RunTask3() {
+        Node<int> root = new Node<int>(1);
+
+        Node<int> child1 = new Node<int>(2);
+        Node<int> child2 = new Node<int>(3);
+        Node<int> child3 = new Node<int>(4);
+
+        Node<int> grandChild1 = new Node<int>(5);
+        Node<int> grandChild2 = new Node<int>(6);
+        Node<int> grandChild3 = new Node<int>(7);
+
+        root.Children.Add(child1);
+        root.Children.Add(child2);
+        root.Children.Add(child3);
+
+        child1.Children.Add(grandChild1);
+        child1.Children.Add(grandChild2);
+
+        child2.Children.Add(grandChild3);
+
+        Console.WriteLine("Значения всех потомков узла root:");
+        root.PrintChildren();
+    }
 }
